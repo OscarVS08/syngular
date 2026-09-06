d62dc7845030286bafb47eefafdd7f865393e6a1
OscarVS08 <oscarvaqueritosa@gmail.com>
2026-09-06 00:30:55 -0600

    feat: initialize spring boot backend structure and dependencies

---
+++ syngular/src/main/java/com/vsolabs/syngular/SyngularApplication.java
@@ -0,0 +1,13 @@
+package com.vsolabs.syngular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyngularApplication.class, args);
+	}
+
+}
---
+++ syngular/src/test/java/com/vsolabs/syngular/SyngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsolabs.syngular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
